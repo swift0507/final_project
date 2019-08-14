b85557203613f6d00cc6be881a8cb6d0a2facc96
usm0507 <usm0507@gmail.com>
2019-08-14 13:52:54 +0900

    serviceimpl dao 수정

--- Final_Project/src/service/HSServiceImpl.java
+++ Final_Project/src/service/HSServiceImpl.java
@@ -3,12 +3,12 @@ package service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import dao.IMemberDao;
+import dao.MemberDao;
 
 @Service
 public class HSServiceImpl implements HSService {
 	
 	@Autowired
-	IMemberDao memberDao; 
+	MemberDao memberDao; 
 	
 }
