3cb317768e168e8588e4b2c707ce0de1aed4ec7e
vastly10 <vastly10@gmail.com>
2019-08-14 15:49:04 +0900

    HSServiceField 생성, ProdOption -> ProdOptionDao로 변경,

--- Final_Project/src/controller/AllController.java
+++ Final_Project/src/controller/AllController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import model.Member;
@@ -16,10 +19,15 @@ public class AllController {
 	@Autowired
 	HSService service;
 	
+	@RequestMapping("loginForm.do")
+	public void loginForm() {}
+	
 	//true면 세션 id에 id 세팅 grade에 등급 세팅하고 true 리턴
 	@RequestMapping("login.do")
 	public @ResponseBody boolean login(Member m, HttpSession session) {
+//		String[] id = req.getParameterValues("id");
+		System.out.println("test");
 		
-		return true;
+		return service.loginCheck(m);
 	}
 }
